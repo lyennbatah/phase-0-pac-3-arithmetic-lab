6ea37fe6e2b5c21a6dbb6bdb847ceb6d4a91270a
+ <+>
2023-08-08 10:15:17 -0400

    calculator answer

---
+++ index.js
@@ -0,0 +1,70 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+function add(){
+    return "something";
+}
+function add(a, b) {
+    return a + b; 
+}
+function subtract(){
+    return "something";
+}
+function subtract(a, b) {
+    return a-b;
+}
+function multiply(){
+    return "something";
+}
+function multiply (a, b) {
+    return a * b;
+}
+function divide(){
+    return "something";
+}
+function divide (a, b) {
+    return a / b;
+}
+function increment(n){}
+function increment (n){
+    return n+1;
+}
+function decrement(n){}
+function decrement(n) {
+   return n-1;
+}
+function makeInt(n){
+    return parseInt(n,10);
+}
+ parseInt (n, "10");
+
+ function preserveDecimal(n){}
+ function preserveDecimal(n){
+    return parseFloat(n);
+ }
+ parseFloat(n);
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
